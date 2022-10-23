a3979caa34e89b41b89e6e3504d04d8230e7aec6
Aylin <92411850+aylinhyusmen@users.noreply.github.com>
2022-10-24 01:23:14 +0300

    Worked on the Header part..

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className='app'><h1>Building a LinkedIn clone</h1>
+
+    {/* Header */}
+    <Header/>
+    {/* App Body */}
+    {/* Sidebar */}
+    {/* Feed */}
+    {/* Widgets */}
+    </div>
+  )
+}
+
+export default App
